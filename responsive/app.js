f01d0d3369d0323d9f00a439b276c33f32a6521f
Andrei Clejanu <doruandrei1998@yahoo.com>
2020-02-23 20:37:58 +0200

    fixed navbar for responsive

--- responsive/app.js
+++ responsive/app.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-  
-
-    /* -----------------------------------------------
+  /* -----------------------------------------------
                     MOBILE NAVIGATION     
     -------------------------------------------------- */
-    /*if($( ".main-nav" ).hasClass( "mobile-hide" )){
+  /*if($( ".main-nav" ).hasClass( "mobile-hide" )){
       $(".main-nav").slideUp(0);
     }*/
 
-    /*while(screen.width < 690) {
+  /*while(screen.width < 690) {
       $(".main-nav").addClass("mobile-hide");
       $(".main-nav").slideUp(0);
   }else{
@@ -17,80 +15,70 @@ $(document).ready(function() {
   }*/
 
   $(document).ready(function() {
-      if($(window).width() >= 690) {
-            $(".main-nav").removeClass("mobile-hide");
-            $(".main-nav").addClass("mobile-show");
+    if ($(window).width() >= 690) {
+      $(".main-nav").removeClass("mobile-hide");
+      $(".main-nav").addClass("mobile-show");
+    } else {
+      $(".main-nav").addClass("mobile-hide");
+      $(".main-nav").slideUp();
+      $(".main-nav").removeClass("mobile-show");
+    }
 
-      } else {
-            $(".main-nav").addClass("mobile-hide");
-            $(".main-nav").slideUp(0);
-            $(".main-nav").removeClass("mobile-show");
-    
-}
-});
-
-/*$(window).on('resize', function(){
+    $(window).on("resize", () => {
       var win = $(this); //this = window
       if (win.width() <= 690) {
-            $(".main-nav").addClass("mobile-hide");
-            $(".main-nav").slideUp(0); 
-            }else{
-            $(".main-nav").removeClass("mobile-hide");
-            $(".main-nav").addClass("mobile-show");
-            $(".main-nav").slideDown(0);   
-});*/
-
-
-$(".burger-icon").on("click", function() {
-      if($( ".main-nav" ).hasClass( "mobile-hide" )){
-            $(".main-nav").removeClass("mobile-hide");
-            $(".main-nav").addClass("mobile-show");
-            $(".main-nav").slideDown(550);
-
-            if($(window).width() > 690) {
-                  $(".main-nav").addClass("mobile-hide");
-                  $(".main-nav").slideUp(0);
-                  $(".main-nav").removeClass("mobile-show");
-            }
-            
-      }else{
-              $(".main-nav").addClass("mobile-hide");
-              $(".main-nav").slideUp(550);
-              $(".main-nav").removeClass("mobile-show");
-              
+        $(".main-nav").slideUp(200, () => {
+          $(".main-nav").addClass("mobile-hide");
+        });
+      } else {
+        $(".main-nav").removeClass("mobile-hide");
+        $(".main-nav").addClass("mobile-show");
+        $(".main-nav").slideDown(0);
       }
-  
-      if($(window).width() > 690) {
-            $(".main-nav").addClass("mobile-hide");
-            $(".main-nav").slideUp(0);
-            $(".main-nav").removeClass("mobile-show");
-      }
+    });
 
+    $(".burger-icon").on("click", function() {
+      if ($(".main-nav").hasClass("mobile-hide")) {
+        $(".main-nav").removeClass("mobile-hide");
+        $(".main-nav").addClass("mobile-show");
+        $(".main-nav").slideDown();
 
-  });
-    
-      $("#bull0").on("click", function() {
-        if($( "#bull0").hasClass( "fa-check-square" )){
-              $("#bull0").removeClass("fa-check-square");
-              $("#bull0").addClass("fa-square");
-        }else{
-                $("#bull0").removeClass("fa-square");
-                $("#bull0").addClass("fa-check-square");
+        if ($(window).width() > 690) {
+          $(".main-nav").addClass("mobile-hide");
+          $(".main-nav").slideUp();
+          $(".main-nav").removeClass("mobile-show");
         }
-    
-  });
+      } else {
+        $(".main-nav").addClass("mobile-hide");
+        $(".main-nav").slideUp();
+        $(".main-nav").removeClass("mobile-show");
+      }
+
+      if ($(window).width() > 690) {
+        $(".main-nav").addClass("mobile-hide");
+        $(".main-nav").slideUp();
+        $(".main-nav").removeClass("mobile-show");
+      }
+    });
+
+    $("#bull0").on("click", function() {
+      if ($("#bull0").hasClass("fa-check-square")) {
+        $("#bull0").removeClass("fa-check-square");
+        $("#bull0").addClass("fa-square");
+      } else {
+        $("#bull0").removeClass("fa-square");
+        $("#bull0").addClass("fa-check-square");
+      }
+    });
 
     $("#bull1").on("click", function() {
-      if($( "#bull1").hasClass( "fa-check-square" )){
-            $("#bull1").removeClass("fa-check-square");
-            $("#bull1").addClass("fa-square");
-      }else{
-              $("#bull1").removeClass("fa-square");
-              $("#bull1").addClass("fa-check-square");
+      if ($("#bull1").hasClass("fa-check-square")) {
+        $("#bull1").removeClass("fa-check-square");
+        $("#bull1").addClass("fa-square");
+      } else {
+        $("#bull1").removeClass("fa-square");
+        $("#bull1").addClass("fa-check-square");
       }
-  
-  });
-
-
-
     });
+  });
+});
