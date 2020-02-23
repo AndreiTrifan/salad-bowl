20f6dfa91b826144ed01041f72b36bb1642326a3
Andrei Clejanu <doruandrei1998@yahoo.com>
2020-02-24 00:04:37 +0200

    Challange Partially  Completed?

--- responsive/app.js
+++ responsive/app.js
@@ -72,12 +72,68 @@ $(document).ready(function() {
     });
 
     $("#bull1").on("click", function() {
+      if ($("#bull1").hasClass("fa - check - square")) {
+        $("#bull1").removeClass("fa-check-square");
+        $("#bull1").addClass("fa-square");
+      } else {
+        $("#bull1").removeClass("fa-square");
+        $("#bull1").addClass("fa-check-square");
+      }
+    });
+
+    $(".submit").on("click", function() {
       if ($("#bull1").hasClass("fa-check-square")) {
         $("#bull1").removeClass("fa-check-square");
         $("#bull1").addClass("fa-square");
       } else {
         $("#bull1").removeClass("fa-square");
         $("#bull1").addClass("fa-check-square");
+      }
+    });
+
+    $(".submit").click(() => {
+      var value = $(".pics").val();
+
+      //       if (value = 1 {
+      //             $(".wrapper").append(`<div class="featured zoom">
+      //             <div id="meta">Author Meta</div>
+      //             <h1>This is a main headline for your most important article.</h1>
+      //             <span id="hero-date">October 2, 2018</span>
+      //           </div>`);
+      // }
+
+      if (value > 1 && value < 4) {
+        //       $(".wrapper").append(`<div class="featured zoom">
+        //       <div id="meta">Author Meta</div>
+        //       <h1>This is a main headline for your most important article.</h1>
+        //       <span id="hero-date">October 2, 2018</span>
+        //     </div>`);
+
+        for (let index = 0; index < value - 1; index++) {
+          $(".sub-featured").append(`<div class="sub1 zoom">
+                          <h3>Another New Thingy</h3>
+                        </div>`);
+        }
+      }
+
+      if (value > 4) {
+        //       $(".wrapper").append(`<div class="featured zoom">
+        //       <div id="meta">Author Meta</div>
+        //       <h1>This is a main headline for your most important article.</h1>
+        //       <span id="hero-date">October 2, 2018</span>
+        //     </div>`);
+
+        for (let index = 0; index < value - 4; index++) {
+          $(".new-featured").append(`<div class="new1 zoom">
+                  <h3>Another New Thingy</h3>
+                </div>`);
+        }
+        value = value - 3;
+        for (let index = 0; index < value; index++) {
+          $(".sub-featured").append(`<div class="sub1 zoom">
+                                    <h3>Another New Thingy</h3>
+                                  </div>`);
+        }
       }
     });
   });
