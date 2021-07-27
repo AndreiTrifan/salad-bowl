e51151c12b1bcaef6860bead071011619e9026fd
pre2andrei <pre4nd1@gmail.com>
2021-07-27 15:13:43 +0300

    dynamic parameter required and null parameter safety

--- lib/utils/error_check_utils.dart
+++ lib/utils/error_check_utils.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-Widget checkWidgetParameter({dynamic parameter, required Widget widget}) {
-  if (parameter == null || (parameter is String && parameter.isEmpty))
-    return Container();
+Widget checkWidgetParameter(
+    {required dynamic parameter, required Widget widget}) {
+  if (parameter == null) return Container();
+
+  if (parameter is String) {
+    if (parameter.isEmpty) return Container();
+  }
   return widget;
 }
