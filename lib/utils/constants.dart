e600cafa42ff6484cdd90f9d92df71bfa30f654c
ivascualexandru <78162360+ivascualexandru@users.noreply.github.com>
2021-07-27 13:37:57 +0300

    #10 Standard Image Aspect Ratio and Image Orientation added
    #10

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,8 @@
-
-enum Environment {
-  PRODUCTION,
-  DEVELOPMENT
-}
+enum Environment { PRODUCTION, DEVELOPMENT }
 
 class Routes {
   static const coupons = 'coupons';
   static const home = 'home';
 }
+
+enum ImageOrientation { LANDSCAPE, PORTRAIT }
--- lib/utils/widgets/standard_image.dart
+++ lib/utils/widgets/standard_image.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import '../standards/style_standards.dart';
+import '../constants.dart';
 
 class StandardImage extends StatefulWidget {
-  const StandardImage({required this.URL});
-  final String URL;
+  /*
+      This widget takes a "url" that points to either the relative path of a 
+      file stored in memory or a web page url. It also takes an 
+      ImageOrientation enum, which is used to display a web image, or to 
+      manually set the aspect ratio of a cached image.
+      
+      In the case that it points to a webpage (e.g. starts with "https://"), 
+      then it makes use of a FadeInImage.network widget to display a 
+      placeholder image while the web image is being loaded. It also checks if 
+      the 
+
+      In the case that it points to a path (e.g. it doesn't start with "https://
+      "), it makes use of an AspectRatio widget, whose child is an Image widget 
+      which finally renders the picture at the URL provided in the function 
+      call.
+    */
+  const StandardImage({required this.url, required this.imageOrientation});
+  final String url;
+  final ImageOrientation imageOrientation;
 
   @override
   _StandardImageState createState() => _StandardImageState();
@@ -18,13 +36,20 @@ class _StandardImageState extends State<StandardImage> {
           borderRadius: BorderRadius.all(
             Radius.circular(StandardCornerRadius.radius),
           )),
-      child: widget.URL.startsWith('https://')
+      child: widget.url.startsWith('https://')
           ? FadeInImage.assetNetwork(
-              placeholder: 'assets/placeHolders/image_placeholder.png',
-              image: widget.URL,
+              placeholder: widget.imageOrientation == ImageOrientation.LANDSCAPE
+                  ? 'assets/placeHolders/image_placeholder_landscape.png'
+                  : 'assets/placeHolders/image_placeholder_portrait.png',
+              image: widget.url,
             )
-          : Image(
-              image: AssetImage(widget.URL),
+          : AspectRatio(
+              aspectRatio: widget.imageOrientation == ImageOrientation.LANDSCAPE
+                  ? 16 / 9
+                  : 5 / 4,
+              child: Image(
+                image: AssetImage(widget.url),
+              ),
             ),
     );
   }
