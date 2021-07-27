766fb5cff2bf8b68023f52bc67492fca44cb647f
ivascualexandru <78162360+ivascualexandru@users.noreply.github.com>
2021-07-27 10:59:41 +0300

    Adaugat functie inaltime text

---
+++ lib/utils/size_measurement_utils.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+double getTextHeight(String text, TextStyle style, double textScaleFactor) {
+  /*
+    Input: Textul care va veni in widget-ul Text, stilul acestui text, si 
+    factorul de scalare al acestuia.
+    Output: Inaltimea widget-ului Text in care se va folosi
+
+    Aceasta functie cere textul dintr-un widget Text, si style-ul pe care il 
+    foloseste, si returneaza inaltimea acestuia, folosindu-se de TextPainter. 
+    Se ia in considerare si o variabila de tip double pe nume textScaleFactor, 
+    care se foloseste dinamic in return statement. Aceasta are documentatii la 
+    https://api.flutter.dev/flutter/widgets/Text/textScaleFactor.html.
+
+    Aceasta functie poate fi si adaptata ca sa returneze si latimea textului, 
+    trebuie doar specificat la functia de return (si sa se schimbe si 
+    numele functiei).
+  */
+  final TextPainter textPainter = TextPainter(
+      text: TextSpan(text: text, style: style),
+      maxLines: 1,
+      textDirection: TextDirection.ltr)
+    ..layout(minWidth: 0, maxWidth: double.infinity);
+  return textPainter.size.height * textScaleFactor;
+}
