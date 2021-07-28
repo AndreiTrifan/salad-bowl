d7e5125eb3c62980cc0cb78aff821102841527d5
iuliacozma <74408668+iuliacozma@users.noreply.github.com>
2021-07-28 12:30:11 +0300

    Update add_margin.dart
    Updated add_margin.dart

--- lib/widgets/add_margin.dart
+++ lib/widgets/add_margin.dart
@@ -4,19 +4,16 @@ import '../utils/standards/style_standards.dart';
 
 class AddMargin extends StatelessWidget {
   final Widget child;
-  final double margin;
 
   AddMargin({
     required this.child,
-    this.margin = StandardMargin.cardMargin,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(
-        left: margin,
-        right: margin,
+      margin: EdgeInsets.symmetric(
+        horizontal: StandardMargin.cardMargin,
       ),
       child: child,
     );
