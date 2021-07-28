efaae65054da68de9d2714d373c214469681401f
pre2andrei <pre4nd1@gmail.com>
2021-07-28 14:41:11 +0300

    refactored request failed screen

--- lib/utils/standards/standard_fail_screen.dart
+++ lib/utils/standards/standard_fail_screen.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:u_win/utils/standards/standard_image.dart';
-import 'package:u_win/utils/standards/style_standards.dart';
+import 'package:u_win/utils/standards/standard_spacing.dart';
+
+import '../../utils/standards/standard_image.dart';
+import '../../utils/standards/style_standards.dart';
 
 class FailedRequestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          StandardImage(
-            url: 'assets/illustrations/warning.png',
-            fit: BoxFit.scaleDown,
-            width: MediaQuery.of(context).size.width / 2,
-          ),
-          SizedBox(height: 40),
-          Text('Datele nu au putut fi incarcate!',
-              style: StandardTextStyles.body.regular)
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        StandardImage(
+          url: 'assets/illustrations/warning.png',
+          width: MediaQuery.of(context).size.width / 2,
+        ),
+        StandardVerticalSpacing(),
+        Text('Datele nu au putut fi incarcate!',
+            style: StandardTextStyles.body.regular)
+      ],
     );
   }
 }
